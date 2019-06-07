07de503531958ab8dd6e1ed4de8512eee9da6bff
Nick <studioo1533@gmail.com>
2019-06-07 11:50:10 +0200

    UpdateMapBoxStyles (#71)

--- src/partials/MapBox/MapBox.tsx
+++ src/partials/MapBox/MapBox.tsx
@@ -5,6 +5,7 @@ import Button from '../Button';
 
 export interface MapBoxProps {
   title?: string;
+  name?: string;
   country?: string;
   text?: string;
   address?: string;
@@ -15,7 +16,6 @@ export interface MapBoxProps {
   storeChief?: string;
   keywords?: string;
   web?: LooseObject;
-  name?: string;
   position?: string;
   url?: LooseObject;
   onClick: any;
